b233599565a4141b0e5e7af20ad6bb07f880eb24
kenneth gil <astronori@gmail.com>
2019-11-25 15:32:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,19 +17,9 @@ line(katz_deli)
 def line_simple(array) # this one follows the same mechanics as learn.co
   current_line = "The simple line is currently:"
   array.each.with_index(1) do |value, indexemus|  
-  # "each.with_index" is the method...must use "index"
-    current_line << " #{indexemus}. #{value},"    
-    # "indexemus" is used to illustrate this variable can be different from "index"
   end 
   puts current_line
 end 
-  
-line_simple(katz_deli)  
-#[:foo, :bar, :baz].each.with_index(1) do |value, index|
-#    puts "#{index}: #{value}"
-#end
-
-
 
 def take_a_number(line, new_person)
   line.push(new_person) # could say: "line << new_person"
